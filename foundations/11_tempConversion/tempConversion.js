ce5448fbf00fd9096bc316e8eb6c7988ed152846
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 10:27:38 +0300

    solved the excercise for calculator.js

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((5 / 9) * (fahrenheit - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((9 / 5) * celsius + 32) * 10) / 10;
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,63 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, item) => {
+    return total * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let factorial_result = 1;
+  if (number === 0) {
+    factorial_result = 1;
+  }
+
+  for (let i = number; i >= 1; --i) {
+    factorial_result *= i;
+  }
+
+  return factorial_result;
 };
+
+add(0, 0);
+add(2, 2);
+add(2, 6);
+
+subtract(10, 4);
+subtract(-10, -4);
+subtract(-8, 7);
+
+sum([]);
+sum([7]);
+sum([7, 11]);
+
+multiply([2, 4]);
+multiply([2, 4, 6, 8, 10, 12, 14]);
+
+power(4, 3);
+power(3, 10);
+
+factorial(0);
+factorial(1);
+factorial(2);
+factorial(5);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +66,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
