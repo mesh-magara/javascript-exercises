a62b0206313d645c5a6b505e80cfc4e5de8c73d8
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 08:49:32 +0300

    practised on repeating the string a numbers of times , excercise was pretty simple

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (name, times) {
+  repeatedWord = "";
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < 3; ++i) {
+      repeatedWord += name;
+    }
+  }
+  return repeatedWord;
 };
 
 // Do not edit below this line
