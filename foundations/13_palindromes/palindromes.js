cc3e1fa15a5ae96e2e0f1a7e3c9234b90ba351d7
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 10:55:44 +0300

    solved the excercise of palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleaned_string = word
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
+
+  let reversed_word = cleaned_string.split("").reverse().join("");
+
+  return cleaned_string === reversed_word;
 };
+
+palindromes("racecar");
+palindromes("rac3e3car");
+palindromes("A car, a man, a maraca.");
+palindromes("racecar!");
+palindromes("ZZZZ car, a man, a maracaz.");
+palindromes("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
