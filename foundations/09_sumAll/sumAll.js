a74876640df26cb46083787b6a185347b73d6d2a
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 09:17:58 +0300

    did summing for range in loops and array

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
 
+  num1 = 1;
+  num2 = 5;
+
+  if (num1 > num2) {
+    for (let i = num1; i >= num2; --i) {
+      sum += i;
+    }
+  } else {
+    for (let i = num1; i <= num2; ++i) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
