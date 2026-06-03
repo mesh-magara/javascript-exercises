40db2a0eaf7767ef6d766b80ef9d719fbe0ff860
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 17:08:49 +0300

    solved the fibonacci series in the simplest way

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number(n);
 
+  fibb_array = [];
+
+  for (let i = 0; i <= n; ++i) {
+    let fibb_number;
+
+    if (i == 0) {
+      fibb_number = 0;
+      fibb_array.push(fibb_number);
+    } else if (i == 1) {
+      fibb_number = 1;
+
+      fibb_array.push(fibb_number);
+    } else {
+      fibb_number = fibb_array[i - 1] + fibb_array[i - 2];
+      fibb_array.push(fibb_number);
+    }
+  }
+
+  if (n < 0) {
+    return "OOPS";
+  } else {
+    return fibb_array[n];
+  }
 };
+fibonacci(4);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
