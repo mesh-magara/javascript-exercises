0f8df0b1d5c1ad289f65ca27c8c4540bcd3a6ab7
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 09:04:18 +0300

    completed the removing items from an array using splice() method

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, num) {
+  arr = [1, 2, 3, 4];
+  num = 3;
+
+  let deleting_index = arr.indexOf(3);
+
+  arr.splice(deleting_index, 1);
+  return arr;
 };
 
 // Do not edit below this line
