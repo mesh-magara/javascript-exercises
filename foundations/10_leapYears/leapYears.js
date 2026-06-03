68ba2906e898dcb94bf58a704aad76356df76eb1
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 09:43:44 +0300

    solved leap year excercise.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //check for the leap year
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
 
+  if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
