5ece8fe9c69dae69a9092e23303296fb1029687d
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 08:50:43 +0300

    created a function to reverse a string pretty simple

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reversed_string = "";
+  string = "hello";
 
+  for (let i = string.length - 1; i >= 0; --i) {
+    reversed_string += string[i];
+  }
+  return reversed_string;
 };
 
 // Do not edit below this line
