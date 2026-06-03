5a33c7fb800df1fbd0c202f8db85acec0465331d
mesh-magara <meshackmagara75@gmail.com>
2026-06-03 11:03:50 +0300

    solved this excercise with so much ease

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  let titles = arr.map((object) => {
+    return object.title;
+  });
+  return titles;
 };
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
