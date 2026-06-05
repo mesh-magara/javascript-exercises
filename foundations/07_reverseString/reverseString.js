001eb92b0cfa5f890cccd4f3dd53c9aa92d031b9
mesh-magara <meshackmagara75@gmail.com>
2026-06-05 19:43:35 +0300

    removed the local varible to only used the passed string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,7 +1,5 @@
 const reverseString = function (string) {
   let reversed_string = "";
-  string = "hello";
-
   for (let i = string.length - 1; i >= 0; --i) {
     reversed_string += string[i];
   }
