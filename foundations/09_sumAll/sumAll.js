c88ec80ca108fdd083b8219c7f08e3691859fbda
mesh-magara <meshackmagara75@gmail.com>
2026-06-05 20:16:50 +0300

    modifies the sumAll function
    changes and modifies the function to also handle the
    negative numbers and the numbers that are also not
    integers or rather string

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,20 +1,25 @@
 const sumAll = function (num1, num2) {
-  let sum = 0;
-
-  num1 = 1;
-  num2 = 5;
-
-  if (num1 > num2) {
-    for (let i = num1; i >= num2; --i) {
-      sum += i;
-    }
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    (num1 || num2) < 0
+  ) {
+    return "ERROR";
   } else {
-    for (let i = num1; i <= num2; ++i) {
-      sum += i;
-    }
-  }
+    let sum = 0;
 
-  return sum;
+    if (num1 > num2) {
+      for (let i = num1; i >= num2; --i) {
+        sum += i;
+      }
+    } else {
+      for (let i = num1; i <= num2; ++i) {
+        sum += i;
+      }
+    }
+
+    return sum;
+  }
 };
 
 // Do not edit below this line
