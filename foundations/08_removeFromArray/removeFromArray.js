051b63c9d28691102490ff1db7f6f5c80d0ed0c7
mesh-magara <meshackmagara75@gmail.com>
2026-06-05 20:01:08 +0300

    modified the removeFromArray function
    modified the function to fiilter out the items
    that are passed in the arguments.The function filters
    and return the items that are passed in the function
    as the argument

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,11 +1,5 @@
-const removeFromArray = function (arr, num) {
-  arr = [1, 2, 3, 4];
-  num = 3;
-
-  let deleting_index = arr.indexOf(3);
-
-  arr.splice(deleting_index, 1);
-  return arr;
+const removeFromArray = function (arr, ...args) {
+  return arr.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
